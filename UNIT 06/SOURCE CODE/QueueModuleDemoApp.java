6964589331604f0e95cdf3f9faac46c737a32cc5
lukhetfo <47589934+lukhetfo@users.noreply.github.com>
2024-09-15 16:17:24 +0200

    Update QueueModuleDemoApp.java

--- UNIT 06/SOURCE CODE/QueueModuleDemoApp.java
+++ UNIT 06/SOURCE CODE/QueueModuleDemoApp.java
@@ -1,7 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *******************************************************
+ * 2019 (C) EL Dube Enterprises
+ * Free and Open Source License Granted by the developer 
+ *
+ * Developed By: Enock Lukhetfo Dube
+ *               Department of Computer Science
+ *               University of Eswatini
+ *******************************************************
  */
 package com.dube.main;
 
